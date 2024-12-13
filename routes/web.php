d6b54643296212cd7c7aab0f5d6653e32bce84ca
MOHSEN <mohsen.mhm23@gmail.com>
2024-12-14 00:23:02 +0330

    Add middleware & permission support

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,7 @@ use Illuminate\Support\Facades\Route;
 use MohsenMhm\LaravelTracking\Http\Controllers\RequestTrackerController;
 use MohsenMhm\LaravelTracking\Http\Middleware\CheckTrackingPermission;
 
-Route::get(config('tracking.route_url', 'request-logs'), [RequestTrackerController::class, 'index'])
-    ->middleware([config('tracking.middleware'), CheckTrackingPermission::class])->name('request.logs');
+Route::middleware(config('tracking.middleware'))->group(function () {
+    Route::get(config('tracking.route_url', 'request-logs'), [RequestTrackerController::class, 'index'])
+        ->middleware([CheckTrackingPermission::class])->name('request.logs');
+});
